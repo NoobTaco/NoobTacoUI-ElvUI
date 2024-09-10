1a214c2610f2c8e446fa42169adf4f7c3afe7819
Mike Norton <mike@corcra.dev>
2024-09-10 09:24:16 -0700

    Refactor code to add integration setup and update file paths
    closes #50

--- integrations/integrations.lua
+++ integrations/integrations.lua
@@ -1,13 +1,8 @@
 local NoobTacoUI, E, L, V, P, G = unpack(select(2, ...))
 
 function NoobTacoUI:SetupIntegration(layout, wowver)
-  -- TODO: Add more bag mod checks
-  if (C_AddOns.IsAddOnLoaded("Baganator")) then
-    E.private["bags"]["enable"] = false
-  end
-
-  -- Check if XIV_Databar is loaded and disable ElvUI databars
-  if (C_AddOns.IsAddOnLoaded("XIV_Databar")) then
-    E.private["databars"]["enable"] = false
-  end
+  NoobTacoUI:IntegrationBag()
+  NoobTacoUI:XIV_Databar()
 end
+
+-- TODO: Cell integration code goes here
